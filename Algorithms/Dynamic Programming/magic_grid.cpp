0a241a7090d373b0321ae9d9b0c73c6b6eae9d5f
Keshav Mishra <mishrakeshav@users.noreply.github.com>
2020-06-28 14:14:50 +0530

    Update

--- Algorithms/Dynamic Programming/magic_grid.cpp
+++ Algorithms/Dynamic Programming/magic_grid.cpp
@@ -1,53 +1,91 @@
 #include <bits/stdc++.h> 
 using namespace std; 
-int magic_grid_recursion(int ** input,int a, int b ,int n, int m){
+/*
+You are given a grid of size m, n. 
+You have to find out the minimum amount of points required at the starting point. 
+to reach from a, b to n , m in the grid. 
+The numbers of the grid can be negative. 
+The number of points at any index cannot be zero or negative
+*/
+int magic_grid_dp(int ** input, int a, int b, int n, int m, int ** output){
+    if(input[n][m] > 0) output[n][m] = 0;
+    else output[n][m] = abs(input[n][m]) + 1;
+
+    for(int i = n-1 ; i >= 0 ; i-- ){
+        if(input[i][m] > 0) output[i][m] = output[i+1][m] ;
+        else output[i][m] = output[i+1][m] + abs(input[i][m]) + 1;
+    } 
+    for(int i = m - 1;i >= 0; i--){
+        if(input[n][i] > 0) output[n][i] = output[n][i+1];
+        else output[n][i] = output[n][i + 1] + abs(input[n][i]) + 1;
+    }
+    int cost;
+    for(int i = n - 1; i >= 0; i--){
+        for(int j = m - 1; j >= 0; j--){
+            cost = min(output[i + 1][j] , output[i][j + 1]);
+            if(input[i][j] > 0) output[i][j] = cost;
+            else output[i][j] = cost + abs(input[i][j]) + 1;
+        }
+    }
+    
+    return output[a][b] - input[a][b];
+}
+int magic_grid_recursion(int ** input,int a, int b ,int n, int m , int ** output){
     
     if(a == n && b == m){
-        if(input[n][m] < 0) return abs(input[n][m]);
-        return 1;
+        if (input[n][m] > 0) output[n][m] = 1;
+        else output[n][m] = abs(input[n][m]) + 1;
+        return output[n][m];
     }
-    if(a > n || b > m){
-        return 0;
-    }
-
-    int cost1 = input[a][b] + magic_grid_recursion(input,a + 1,b,n,m);
-    int cost2 = input[a][b] + magic_grid_recursion(input,a,b + 1,n,m);
-    if(cost1 != INT_MAX && cost2 != INT_MAX){
-        if(cost1 < 0){
-            cost1 = abs(cost1);
+    if(a < n and b < n){
+        int cost1 = magic_grid_recursion(input,a + 1,b,n,m, output);
+        int cost2 = magic_grid_recursion(input, a, b + 1, n , m,output);
+        if(input[a][b] > 0){
+            if(input[a][b] > cost1){
+                cost1 = 1;
+            }
+            else{
+                cost1 -= input[a][b];
+            }
+            if(input[a][b] > cost2){
+                cost2 = 1;
+            }
+            else{
+                cost2 -= input[a][b];
+            }
         }
-        else if(cost2 < 0){
-            cost2 = abs(cost2);
+        else{
+            cost1 += abs(input[a][b]);
+            cost2 += abs(input[a][b]);
         }
-        if(cost1 == 0){
-            cost1 = 1;
-        }
-        if(cost2 == 0){
-            cost2 = 1;
-        }
+        output[a][b] = min(cost1,cost2);
         return min(cost1,cost2);
     }
-    else if(cost1 !=INT_MAX){
-        if(cost1 < 0){
-            cost1 = abs(cost1);
+    else{
+        int cost;
+        if(a == n){
+            cost = magic_grid_recursion(input,a,b+1,n,m,output);
         }
-        if(cost1 == 0){
-            cost1 = 1;
+        if(b == m){
+            cost = magic_grid_recursion(input,a + 1,b, n ,m, output);
+            
         }
-        return cost1;
+        if(input[a][b] > 0){
+            if(input[a][b] > cost){
+                cost = 1;
+            }
+            else{
+                cost -= input[a][b] ;
+            }
+
+        }
+        else{
+            cost += abs(input[a][b]);
+        }
+        output[a][b] = cost;
+        return cost;
     }
-    else if(cost2 != INT_MAX){
-        if(cost2 < 0){
-            cost2 = abs(cost2);
-        }
-        if(cost2 == 0){
-            cost2 = 1;
-        }
-        return cost2;
-    }
-    // else{
-    //     return 1;
-    // }
+
 }
 void solve(); 
 int main() 
@@ -55,11 +93,11 @@ int main()
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL); 
   
-// #ifndef ONLINE_JUDGE 
-//     freopen("input.txt", "r", stdin); 
-//     freopen("error.txt", "w", stderr); 
-//     freopen("output.txt", "w", stdout); 
-// #endif 
+#ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin); 
+    freopen("error.txt", "w", stderr); 
+    freopen("output.txt", "w", stdout); 
+#endif 
   
     int t = 1; 
     cin >> t; 
@@ -70,7 +108,7 @@ int main()
     
     } 
   
-    // cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl; 
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl; 
     return 0; 
 } 
 void solve() 
@@ -81,14 +119,39 @@ void solve()
     for(int i=0; i<n;i++){
         input[i] = new int[m];
     }
+    int ** output = new int*[n];
+    for(int i=0; i<n;i++){
+        output[i] = new int[m];
+    }
     for(int i = 0; i < n; i++){
         for(int j =0; j < m; j++){
             cin >> input[i][j];
         }
     }
     cin >> a >> b;
-    // cout << "Hello World" << endl;
-    int ans =  magic_grid_recursion(input,a,b,n-1,m-1);
-    ans = 1;
+    
+    int ans =  magic_grid_recursion(input,a,b,n-1,m-1, output);
+    // ans = 1;
     cout << ans << endl;
+    for(int i = 0; i < n; i++){
+        for(int j = 0 ; j < m; j++){
+            cout << output[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << a << " " << b << endl;
+    int ** dp = new int*[n];
+    for(int i=0; i<n;i++){
+        dp[i] = new int[m];
+    }
+    ans = magic_grid_dp(input, a, b, n-1 , m-1 , dp);
+    cout << ans << endl;
+    for(int i = 0; i < n; i++){
+        for(int j = 0 ; j < m; j++){
+            cout << dp[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+
 } 

c85637d2c6ede3cd6872296c63414beab9f6bba3
Keshav Mishra <mishrakeshav@users.noreply.github.com>
2020-07-28 22:20:59 +0530

    cses searching and sorting problems

---
+++ CSES Problem Set/Sorting and Searching/concert_tickets.py
@@ -0,0 +1,18 @@
+
+
+
+def solve():
+    h.sort(reverse = True)
+    
+    
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    h = list(map(int, input().split()))
+    t = list(map(int, input().split()))
+    new_h = []
+    for i in range(n):
+        new_h.append([h[i],0])
+    solve()
+    

3a87635b13611b5839966ba421fa704db5f5e8c8
Keshav Mishra <keshav.sm@somaiya.edu>
2020-08-23 13:11:36 +0530

    update

---
+++ AlgoTalks/Linear DS/maximum_xor_secondary.py
@@ -0,0 +1,33 @@
+"""
+Problem link: https://codeforces.com/problemset/problem/281/D
+Solution By Keshav Mishra 
+"""
+from sys import stdin,stdout
+def input(): return stdin.readline().strip()
+# def print(s): stdout.write(str(s)+'\n')
+from collections import Counter 
+
+# Constants 
+YES = 'YES'
+NO = 'NO'
+yes = 'yes'
+no = 'no'
+true = 'true'
+false = 'false'
+
+
+
+
+def solve():
+    n = int(input())
+    s = list(map(int, input().split()))
+    
+
+    
+
+if __name__ == '__main__':
+    
+    ans = solve()
+    if ans:
+        print(ans)
+    

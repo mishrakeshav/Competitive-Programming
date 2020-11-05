27172f25cdc5585ebb3b192d7ff339f2fc659254
soniyamishra <mishrasoniya1999@gmail.com>
2020-11-05 12:07:15 +0530

    update python template

--- template.py
+++ template.py
@@ -5,55 +5,7 @@ Solution By Keshav Mishra
 from sys import stdin,stdout
 from collections import Counter , deque
 from queue import PriorityQueue
-import math
-
-
-helperConstants = True 
-helperUtilityFunctions = True 
-
-def input(): return stdin.readline().strip()
-# def print(s): stdout.write(str(s)+'\n')
-if helperConstants:
-    YES = 'YES'
-    NO = 'NO'
-    yes = 'yes'
-    no = 'no'
-    true = 'true'
-    false = 'false'
-    FALSE = 'FALSE'
-    TRUE = 'TRUE'
-    IMPOSSIBLE = 'IMPOSSIBLE'
-    POSSIBLE = 'POSSIBLE'
-    INF = float('inf')
-
-if helperUtilityFunctions:
-    # Input utility functions
-    def getInputArray():
-        return list(map(int, input().split()))
-    def getIntegerInputs():
-        return map(int, input().split())
-    def getInputIntegerMatrix(n):
-        matrix = []
-        for i in range(n):
-            matrix.append(list(map(int,input().split())))
-        return matrix
-    def getInputStringMatrix(n):
-        matrix = []
-        for i in range(n):
-            matrix.append(input())
-        return matrix
-
-    # Output Utility functions
-    def outputIntegerMatrix(matrix):
-        for i in range(len(matrix)):
-            print(*matrix[i])
-    def outputStringMatrix(matrix):
-        for i in range(len(matrix)):
-            print(matrix[i])
-    def kickstartoutput(testcase,*outputs):
-        print('Case #%d:'%(testcase), *outputs)
-    
-    
+import math    
 
 def solve(t):
     pass 

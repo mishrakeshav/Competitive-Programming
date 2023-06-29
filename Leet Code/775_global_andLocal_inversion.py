3c03140a05ac55191b120de6303c87aeef18f34a
mishrakeshav <keshav.sm@gmail.com>
2023-06-29 20:56:37 +0530

    Empty Leetcode folder

--- Leet Code/775_global_andLocal_inversion.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isIdealPermutation(self, nums: List[int]) -> bool:
-        n = len(nums)
-        for i in range(n):
-            if abs(nums[i]-i) > 1:
-                return False 
-        return True 
-        
-        
